bec199ef8176f66723f9fbb7317ac5c91a9c8df7
unknown <mehiopsss@gmail.com>
2021-04-22 20:20:24 +0545

    Release

---
+++ app.py
@@ -0,0 +1,13 @@
+from flas import Flask, jsonift, request
+import time
+
+app = Flask(__name__)
+@app.route('/bot',method=['POST'])
+
+#response
+def response():
+    query = dict(request.form)['query']
+    result = query + " " + time.ctime()
+    return jsonify{{"response" : result}}
+
+if __name__ == "__main__"
